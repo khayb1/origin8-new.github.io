a84032b1214d1327ef65ab44986e4d493e66a009
Kelvin Osei <129594460+khayb1@users.noreply.github.com>
2024-01-18 18:30:10 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ var carousel = new Swiper(".carousel", {
 
       var copy = document.querySelector(".new-client-wrapper").cloneNode(true);
       document.querySelector(".new-client").appendChild(copy);
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+    
